c17d111c55ceadc3f71c7ce574aa66177881ddb1
Kirill <lechnev@mail.ru>
2023-12-15 22:27:38 +0300

    первый коммит

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Tets");
+    }
+
+
+
+    public  int calc (int x, int y)
+        {
+            return x + y;
+        }
+}
